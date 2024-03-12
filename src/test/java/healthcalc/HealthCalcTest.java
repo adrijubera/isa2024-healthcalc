85cef3808460c0a2dad1d3339a143a401479259e
Adriana Jubera <adrianajubera@MacBook-Pro-de-Adriana.local>
2024-03-12 18:34:11 +0100

    Implementar casos de prueba en HealthCalcTest

--- src/test/java/healthcalc/HealthCalcTest.java
+++ src/test/java/healthcalc/HealthCalcTest.java
@@ -1,6 +1,7 @@
 package healthcalc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,51 @@ import org.junit.jupiter.api.Test;
 public class HealthCalcTest {
 
 	@Test
-	@DisplayName("Esto es un test de ejemplo.")
-	public void bmi() {
-		assertEquals(true, true);
-	}
+    @DisplayName("Prueba para calcular el peso ideal de un hombre.")
+    public void testIdealWeightForMale() {
+        HealthCalc healthCalc = new HealthCalcImpl();
+        try {
+            float result = healthCalc.idealWeight(180, 'm');
+            assertEquals(72.5f, result, 0.01f); // Tolerancia de 0.01f
+        } catch (Exception e) {
+            fail("Excepción lanzada: " + e.getMessage());
+        }
+    }
+	
+	@Test
+    @DisplayName("Prueba para calcular el peso ideal de una mujer.")
+    public void testIdealWeightForFemale() {
+        HealthCalc healthCalc = new HealthCalcImpl();
+        try {
+            float result = healthCalc.idealWeight(160, 'w');
+            assertEquals(56.0f, result, 0.01f); // Tolerancia de 0.01f
+        } catch (Exception e) {
+            fail("Excepción lanzada: " + e.getMessage());
+        }
+    }
+	
+	@Test
+    @DisplayName("Prueba para calcular la tasa metabólica basal de un hombre.")
+    public void testBasalMetabolicRateForMale() {
+        HealthCalc healthCalc = new HealthCalcImpl();
+        try {
+            float result = healthCalc.basalMetabolicRate(70, 180, 'm', 30);
+            assertEquals(1680.0f, result, 0.01f); // Tolerancia de 0.01f
+        } catch (Exception e) {
+            fail("Excepción lanzada: " + e.getMessage());
+        }
+    }
+	
+	@Test
+    @DisplayName("Prueba para calcular la tasa metabólica basal de una mujer.")
+    public void testBasalMetabolicRateForFemale() {
+        HealthCalc healthCalc = new HealthCalcImpl();
+        try {
+            float result = healthCalc.basalMetabolicRate(60, 160, 'w', 25);
+            assertEquals(1314.0f, result, 0.01f); // Tolerancia de 0.01f
+        } catch (Exception e) {
+            fail("Excepción lanzada: " + e.getMessage());
+        }
+    }
+	
 }
